45c819f709501741d31afb13eef275b25284b5ab
gerardo.ureta <gerardo.ureta@sii.cl>
2024-11-21 12:08:41 -0300

    Subo nuevas operaciones

--- src/app/calculadora.ts
+++ src/app/calculadora.ts
@@ -3,6 +3,14 @@ function operar(operacion: string = "", a: number, b: number) {
         return suma(a, b);
     } else if (operacion === 'resta') {
         return restar(a, b);
+    } else if (operacion === 'multiplicacion') {
+        return multiplicacion(a, b);
+    } else if (operacion === 'division') {
+        return division(a, b);
+    } else if (operacion === 'potencia') {
+        return potencia(a, b);
+    } else if (operacion === 'factorial') {
+        return factorial(a);
     }
 }
 
@@ -30,4 +38,64 @@ function restar(a: number, b: number) {
     return a - b;
 }
 
-export { suma, operar, restar };
+function multiplicacion(a: number, b: number) { //a = 1 , b=2
+
+    if (a === undefined || b === undefined) {
+        throw new Error("No se puede multiplicar indefinidos");
+    }
+
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return NaN;
+    }
+
+    return a * b;
+}
+
+function division(a: number, b: number) { //a = 1 , b=2
+
+    if (a === undefined || b === undefined) {
+        throw new Error("No se puede dividir indefinidos");
+    }
+
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return NaN;
+    }
+
+    return a / b;
+}
+
+function potencia(a: number, b: number) { //a = 1 , b=2
+
+    if (a === undefined || b === undefined) {
+        throw new Error("No se puede elevar a potencia numeros indefinidos");
+    }
+
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return NaN;
+    }
+
+    return a**b;
+}
+
+function factorial(a: number) { //a = 1
+
+    if (a === undefined) {
+        throw new Error("No se puede ejecutar factorial numero indefinido");
+    }
+
+    if (typeof a !== 'number') {
+        return NaN;
+    }
+    
+    if (a === 0 || a === 1) {
+        return 1;
+    }
+
+    let nfac = 1;
+    for (let i = 2; i <= a; i++) {
+        nfac = nfac * i;
+    }
+    return nfac;
+}
+
+export { suma, operar, restar, multiplicacion, division, potencia, factorial };
